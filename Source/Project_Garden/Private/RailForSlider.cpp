db8bfefed7c32fadd27b470ebb019b6457ec3260
RxTeK <theodep34@gmail.com>
2025-09-09 13:24:00 +0200

    add railSlider in cpp

---
+++ Source/Project_Garden/Private/RailForSlider.cpp
@@ -0,0 +1,181 @@
+#include "RailForSlider.h"
+#include "Kismet/GameplayStatics.h"
+#include "MyProject8Character.h"
+#include "VectorTypes.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+#include "Engine/World.h"
+
+ARailForSlider::ARailForSlider()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Root
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	// Spline
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
+	SplineComponent->SetupAttachment(RootComponent);
+
+	// Box
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	BoxComponent->SetupAttachment(SplineComponent);
+
+	// Taille et collision par défaut
+	BoxComponent->SetBoxExtent(FVector(32.f, 32.f, 32.f));
+BoxComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxComponent->SetCollisionObjectType(ECC_WorldDynamic);
+	BoxComponent->SetCollisionResponseToAllChannels(ECR_Overlap);
+	BoxComponent->SetGenerateOverlapEvents(true);
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ARailForSlider::OnBoxBeginOverlap);
+}
+
+void ARailForSlider::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	GenerateMeshes();
+}
+
+void ARailForSlider::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	BoxMovement(DeltaTime);
+	MovementOnSpline();
+}
+
+void ARailForSlider::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ARailForSlider::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+									  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+									  bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor) return;
+
+	AMyProject8Character* Player = Cast<AMyProject8Character>(OtherActor);
+	if (!Player) return;
+
+	PlayerRef = Player;
+	bAttach = true;
+
+	// Calculer la distance sur la spline **depuis la position actuelle du joueur**
+	Distance = SplineComponent->GetDistanceAlongSplineAtLocation(PlayerRef->GetActorLocation(),
+																 ESplineCoordinateSpace::World);
+
+	// Déterminer le signe selon la direction du joueur
+	FVector PlayerForward = PlayerRef->GetActorForwardVector();
+	FVector SplineDir = SplineComponent->GetLocationAtDistanceAlongSpline(Distance + 1, ESplineCoordinateSpace::World)
+					   - SplineComponent->GetLocationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+
+	Sign = (FVector::DotProduct(PlayerForward, SplineDir) > 0) ? 1.0f : -1.0f;
+
+	// Stopper le mouvement du joueur
+	if (PlayerRef->GetCharacterMovement())
+	{
+		PlayerRef->GetCharacterMovement()->SetMovementMode(MOVE_None);
+	}
+}
+
+
+void ARailForSlider::GenerateMeshes()
+{
+	// Nettoyer d’anciens composants
+	TArray<USplineMeshComponent*> OldMeshes;
+	GetComponents(OldMeshes);
+	for (auto* Comp : OldMeshes)
+	{
+		Comp->DestroyComponent();
+	}
+
+	if (!MeshToUse || !SplineComponent) return;
+
+	const float SplineLength = SplineComponent->GetSplineLength();
+	float Distancer = 0.0f;
+
+	while (Distancer < SplineLength)
+	{
+		const FVector StartPos = SplineComponent->GetLocationAtDistanceAlongSpline(Distancer, ESplineCoordinateSpace::Local);
+		const FVector StartTangent = SplineComponent->GetTangentAtDistanceAlongSpline(Distancer, ESplineCoordinateSpace::Local);
+
+		Distancer += StepDistance;
+
+		const FVector EndPos = SplineComponent->GetLocationAtDistanceAlongSpline(Distancer, ESplineCoordinateSpace::Local);
+		const FVector EndTangent = SplineComponent->GetTangentAtDistanceAlongSpline(Distancer, ESplineCoordinateSpace::Local);
+
+		USplineMeshComponent* SplineMesh = NewObject<USplineMeshComponent>(this);
+		if (SplineMesh)
+		{
+			SplineMesh->RegisterComponent();
+			SplineMesh->SetStaticMesh(MeshToUse);
+			SplineMesh->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
+			SplineMesh->SetStartAndEnd(StartPos, StartTangent, EndPos, EndTangent);
+		}
+	}
+}
+
+void ARailForSlider::BoxMovement(float DeltaTime)
+{
+	ACharacter* PlayerChar = UGameplayStatics::GetPlayerCharacter(this, 0);
+	if (!PlayerChar) return;
+
+	FVector PlayerLocation = PlayerChar->GetActorLocation();
+
+	float MovementAmount = DeltaTime * SpeedOnSpline;
+
+	FVector NewLocation = PlayerLocation + FVector(MovementAmount, 0.f, 0.f);
+
+	BoxComponent->SetWorldLocation(
+		SplineComponent->FindLocationClosestToWorldLocation(NewLocation, ESplineCoordinateSpace::World)
+	);
+}
+
+void ARailForSlider::MovementOnSpline()
+{
+	if (bAttach && PlayerRef)
+	{
+		PlayerRef->SetActorLocation(SplineComponent->GetLocationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World));
+		Distance += Sign * 5.0f;
+
+		// Vérifier si on est au bout de la spline
+		if (Distance >= SplineComponent->GetSplineLength() || Distance <= 0.0f)
+		{
+			DetachPlayer();
+		}
+	}
+	else if (PlayerRef)
+	{
+	
+		Distance = SplineComponent->GetDistanceAlongSplineAtLocation(BoxComponent->GetComponentLocation(), ESplineCoordinateSpace::World);
+		FVector PlayerForward = PlayerRef->GetActorForwardVector();
+		FVector SplinePos = SplineComponent->GetLocationAtDistanceAlongSpline(Distance + 1, ESplineCoordinateSpace::World) -
+							SplineComponent->GetLocationAtDistanceAlongSpline(Distance, ESplineCoordinateSpace::World);
+		FVector SplinePosFinal = SplinePos.GetSafeNormal();
+		float Dot = FVector::DotProduct(PlayerForward, SplinePosFinal);
+
+		Dot = (Dot > 0) ? Dot + 1.0f : Dot - 1.0f;
+
+		Sign = (Distance >= SplineComponent->GetSplineLength()) ? -1 : Dot;
+	}
+}
+
+void ARailForSlider::DetachPlayer()
+{
+	if (!PlayerRef) return;
+
+	bAttach = false;
+	
+	if (PlayerRef->GetCharacterMovement())
+	{
+		PlayerRef->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+	}
+	
+	FVector DetachImpulse = PlayerRef->GetActorForwardVector() * 300.0f + FVector(0,0,200.f);
+	PlayerRef->LaunchCharacter(DetachImpulse, true, true);
+
+	PlayerRef = nullptr; // plus de référence, sécurité
+}
+
---
+++ Source/Project_Garden/Public/RailForSlider.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "RailForSlider.generated.h"
+
+class AMyProject8Character;
+UCLASS()
+class PROJECT_GARDEN_API ARailForSlider : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARailForSlider();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	/** Spline */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USplineComponent* SplineComponent;
+
+	/** Box attachée à la spline */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spline")
+	UStaticMesh* MeshToUse;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spline")
+	float StepDistance = 100.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spline")
+	float SpeedOnSpline = 1000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Actor")
+	AMyProject8Character* PlayerRef;
+
+	UPROPERTY()
+	bool bAttach = false;
+
+	UPROPERTY()
+	float Distance = 0;
+
+	UPROPERTY()
+	float Sign = 0;
+
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void DetachPlayer();
+	
+private:
+	void GenerateMeshes();
+	void BoxMovement(float DeltaTime);
+	void MovementOnSpline();
+};

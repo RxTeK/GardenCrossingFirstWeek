f578b8cd1f9eb0662ad80fcc3313fc9cb9a43780
RxTeK³ <theodep34@gmail.com>
2025-09-30 08:59:36 +0200

    start boatcpp

---
+++ Source/Project_Garden/Private/Boat.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Boat.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ABoat::ABoat()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	RootDefault = CreateDefaultSubobject<USceneComponent>(TEXT("Mesh"));
+	SetRootComponent(RootDefault);
+	
+	BoatRoot = CreateDefaultSubobject<USceneComponent>(TEXT("BoatRoot"));
+	BoatRoot->SetupAttachment(RootComponent);
+
+	BoatMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BoatMesh"));
+	BoatMesh->SetupAttachment(BoatRoot);
+
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	Collider->InitBoxExtent(FVector(100.f, 100.f, 100.f));
+	Collider->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	Collider->SetGenerateOverlapEvents(true);
+	Collider->SetupAttachment(BoatMesh);
+	if (Collider)
+	{
+		Collider->OnComponentBeginOverlap.AddDynamic(this, &ABoat::OnComponentOverlap);
+	}
+}
+
+// Called when the game starts or when spawned
+void ABoat::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABoat::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABoat::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ABoat::OnComponentOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AMyProject8Character* Chararef = Cast<AMyProject8Character>(OtherActor);
+	if (Chararef && DoOnce == false)
+	{
+		DoOnce = true;
+		Controller = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		Controller->Possess(this);
+		
+		FTimerDelegate TimerDel;
+		TimerDel.BindUFunction(this, FName("MoveBoat"));
+
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle,TimerDel,GetWorld()->GetDeltaSeconds(),true);
+	}
+}
+
+void ABoat::MoveBoat()
+{
+	
+}
+
---
+++ Source/Project_Garden/Public/Boat.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/BoxComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "MyProject8Character.h"
+#include "Boat.generated.h"
+
+UCLASS()
+class PROJECT_GARDEN_API ABoat : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABoat();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	USceneComponent* RootDefault;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoatRoot;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BoatMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* Collider;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void OnComponentOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	bool DoOnce = false;
+
+private:
+	FTimerHandle TimerHandle;
+
+	UPROPERTY(EditAnywhere)
+	USplineComponent* Spline;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 200.f;
+
+	UPROPERTY(EditAnywhere)
+	float LateralOffset = 0.f;
+
+	UPROPERTY(EditAnywhere)
+	float MaxOffset = 100.f;
+
+	float DistanceAlongSpline = 0.f;
+
+	UFUNCTION()
+	void MoveBoat();
+	
+
+};
